bd4e623bedb26944f420988bfc8ef6c10ba30629
Ankita248 <102206335+Ankita248@users.noreply.github.com>
2025-03-04 11:10:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (6.45%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if (n <= 0) return false;
-        while (n % 3 == 0) {
-            n /= 3;
-        }
-        return n==1;
+        double logres=log10(n)/log10(3);
+        return logres==floor(logres);
     }
 };
